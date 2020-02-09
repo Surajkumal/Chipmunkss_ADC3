45af69b0482f6403c4c12ca28fadd4b2def72378
Rikita <amaz.ravishingaarzu@gmail.com>
2020-02-09 13:50:03 +0545

    date

--- Travel_Agency/packages/tests.py
+++ Travel_Agency/packages/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+from .models import Packages
+
+from datetime import datetime
+
 # Create your tests here.
+class PackageTest(TestCase):
+	def setUp(self):
+		Package.objects.create(title="Holiday",
+			name="Sukute Beach", date=datetime.now()
+
+			)
+
+
+	def test_ORM(self):
+		p = Package.objects.get(title="Holiday")
+		self.assertEqual(p.title, "Holiday")	
