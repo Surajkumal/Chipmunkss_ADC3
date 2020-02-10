a2c00996b319d56e2325d89c562df9aef19dc731
JayantKarmacharya <58836920+JayantKarmacharya@users.noreply.github.com>
2020-02-10 15:27:08 +0545

    updatw

--- Travel_Agency/cabs/models.py
+++ Travel_Agency/cabs/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Cabbing(models.Model):
     Pickup = models.CharField(max_length=150)
     Drop = models.CharField(max_length=150)
-    File = models.FileField(upload_to="photo/")
+    File = models.FileField(upload_to="photo/",null=True)
 
     def __str__(self):
         return self.Pickup
+
