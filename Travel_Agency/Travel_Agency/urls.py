994d84b7f3a68c07a7225da484b27ad7b0a88bcb
Samir Husen <59054382+samirhusen@users.noreply.github.com>
2020-02-10 22:58:46 +0545

    update

--- Travel_Agency/Travel_Agency/urls.py
+++ Travel_Agency/Travel_Agency/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     #path(r'^search/',include('Hotel.urls',namespace='search')),
     path('', include('Flights.urls')),
     path('', include('cabs.urls')),
+    
 ]
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -20,10 +21,6 @@ urlpatterns = [
 #     urlpatterns= urlpatterns+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 #     urlpatterns= urlpatterns+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
     
-    
-    
-    
-
 
 #urlpatterns += static(settings.MEDIA_URL,
     #document_root =settings.MEDIA_ROOT)
